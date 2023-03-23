bfe468aa13a37ca90731cbfadb997122d9b3579d
Robin Vobruba <hoijui.quaero@gmail.com>
2023-03-23 21:13:42 +0100

    Updates two copyright year ranges [minor]

--- src/reporters/md_table.nim
+++ src/reporters/md_table.nim
@@ -1,7 +1,7 @@
 # This file is part of osh-tool.
 # <https://github.com/hoijui/osh-tool>
 #
-# SPDX-FileCopyrightText: 2021-2022 Robin Vobruba <hoijui.quaero@gmail.com>
+# SPDX-FileCopyrightText: 2021-2023 Robin Vobruba <hoijui.quaero@gmail.com>
 #
 # SPDX-License-Identifier: AGPL-3.0-or-later
 
